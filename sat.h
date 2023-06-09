e8700712b18ac5386b3a119e83090e67f370dff2
Evan Pacini <evan.pacini@gmail.com>
2023-06-09 13:31:04 +0200

    feat: Summed Area Table from PGM

--- sat.h
+++ sat.h
@@ -37,13 +37,53 @@ SummedAreaTable *AllocateSat(uint32_t width, uint32_t height) {
 }
 
 /**
+ * Compute the summed area table of a PGM image.
+ *
+ * @param pgm   The PGM image.
+ * @return      The summed area table, or NULL if an error occurred.
+ */
+SummedAreaTable *PgmToSat(PgmImage *pgm) {
+  uint32_t width  = pgm->width_;
+  uint32_t height = pgm->height_;
+
+  // Allocate memory for summed area table
+  SummedAreaTable *sat = AllocateSat(width, height);
+  if (!sat) {
+    return NULL;
+  }
+
+#pragma omp parallel for default(none) shared(sat, pgm, width)
+  // Copy first row
+  for (uint32_t x = 0; x < width; x++) {
+    sat->data_[x] = (uint64_t)pgm->data_[x];
+  }
+
+  // Column-wise sum
+  for (uint32_t y = 1; y < height; y++) {
+#pragma omp parallel for default(none) shared(sat, pgm, width, y)
+    for (uint32_t x = 0; x < width; x++) {
+      sat->data_[y * width + x] =
+          (uint64_t)pgm->data_[y * width + x] + sat->data_[(y - 1) * width + x];
+    }
+  }
+#pragma omp parallel for default(none) shared(sat, width, height)
+  //  Row-wise sum
+  for (uint32_t y = 0; y < height; y++) {
+    for (uint32_t x = 1; x < width; x++) {
+      sat->data_[y * width + x] += sat->data_[y * width + x - 1];
+    }
+  }
+  return sat;
+}
+
+/**
  * Free memory for a summed area table.
  *
  * @param sat  The summed area table to free.
  */
 void FreeSat(SummedAreaTable *sat) {
-    free(sat->data_);
-    free(sat);
+  free(sat->data_);
+  free(sat);
 }
 
 #endif// NETPBM_INPUT_SAT_H_
