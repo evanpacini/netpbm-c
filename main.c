151dd43024fdad2f4d3e51cc2e9d7f75b44c1a68
Restyled.io <commits@restyled.io>
2023-09-08 09:05:19 +0000

    Restyled by clang-format

--- main.c
+++ main.c
@@ -5,23 +5,23 @@
 #include "threshold_maps.h"
 
 int main(void) {
-    // Read image and convert to grayscale
-    PpmImage *image     = ReadPpm("../input/tud1.ppm");
-    PgmImage *pgm_image = PpmToPgm(image, SRgbLuminance);
-    FreePpm(image);
+  // Read image and convert to grayscale
+  PpmImage *image     = ReadPpm("../input/tud1.ppm");
+  PgmImage *pgm_image = PpmToPgm(image, SRgbLuminance);
+  FreePpm(image);
 
-    // Get blue noise map
-    PgmImage *blue_noise = ReadPgm("../textures/blue_noise/1024x1024/0.pgm");
-    ThresholdMap *map    = PgmToThresholdMap(blue_noise);
-    FreePgm(blue_noise);
+  // Get blue noise map
+  PgmImage *blue_noise = ReadPgm("../textures/blue_noise/1024x1024/0.pgm");
+  ThresholdMap *map    = PgmToThresholdMap(blue_noise);
+  FreePgm(blue_noise);
 
-    // Convert to PBM
-    PbmImage *pbm = PgmToPbmOrdered(pgm_image, map);
-    FreePgm(pgm_image);
+  // Convert to PBM
+  PbmImage *pbm = PgmToPbmOrdered(pgm_image, map);
+  FreePgm(pgm_image);
 
-    // Write image and free memory
-    WritePbm("../output/test.pbm", pbm);
-    FreePbm(pbm);
-    FreeThresholdMap(map);
-    return 0;
+  // Write image and free memory
+  WritePbm("../output/test.pbm", pbm);
+  FreePbm(pbm);
+  FreeThresholdMap(map);
+  return 0;
 }
